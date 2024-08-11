df9215549053934f16c73ea0bd757ec9e6ec85a8
naushadstudy1993 <naushadstudy1993@gmail.com>
2024-08-11 22:31:24 +0400

    Update App.scala
    Remote - Commit

--- spark-3.0-poc/src/main/scala/spark/poc/App.scala
+++ spark-3.0-poc/src/main/scala/spark/poc/App.scala
@@ -9,7 +9,7 @@ object App {
   def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
-    println( "Hello World!" )
+    println( "Hello World!!!" )
     println("concat arguments = " + foo(args))
   }
 
